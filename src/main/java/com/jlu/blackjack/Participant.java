df93b1e9b7cb0508d49c5ab739edd45a8d925ad7
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 16:00:50 -0500

    Adjust methods in Participant interface

--- src/main/java/com/jlu/blackjack/Participant.java
+++ src/main/java/com/jlu/blackjack/Participant.java
@@ -4,10 +4,28 @@ import java.util.List;
 
 public interface Participant {
 
+    /**
+     * Gets the participant's name in String format
+     * @return String representing the participant's name
+     */
     String getName();
+
+    /**
+     * Check whether the participant is still active in the current round
+     * @return boolean value that describes the active state
+     */
     boolean isStillActive();
+
+    /**
+     * Get the participant's current (main) hand
+     * @return a Hand object that is the main hand
+     */
     Hand getCurrentHand();
+
+    /**
+     * Get the participant's secondary hand in case of a split
+     * @return a Hand object that is the split hand (if any)
+     */
     Hand getSplitHand();
-    void getAction();
 
 }
