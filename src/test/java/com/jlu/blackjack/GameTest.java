28f306ba7c427d4262f97c8aa74581abf00918ec
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 17:37:22 -0500

    Generate basic tests for Game constructors

---
+++ src/test/java/com/jlu/blackjack/GameTest.java
@@ -0,0 +1,25 @@
+package com.jlu.blackjack;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @BeforeEach
+    void setUp() {
+        Rules customRules = new Rules();
+    }
+
+    @Test
+    void emptyConstructor(){
+        fail("Test not implemented.");
+    }
+
+    @Test
+    void customRulesConstructor(){
+        fail("Test not implemented.");
+    }
+
+}
