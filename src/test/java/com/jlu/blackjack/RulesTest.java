03f1534bfe76aaf9714cbe439793448c3b5e415d
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 18:20:30 -0500

    Removed trivial unit tests in RulesTest

--- src/test/java/com/jlu/blackjack/RulesTest.java
+++ src/test/java/com/jlu/blackjack/RulesTest.java
@@ -1,5 +1,6 @@
 package com.jlu.blackjack;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,98 +15,5 @@ class RulesTest {
         testRules = new Rules();
     }
 
-    @Test
-    void getFormattedRules() {
-        fail();
-    }
 
-    @Test
-    void isInsurance() {
-        fail();
-    }
-
-    @Test
-    void setInsurance() {
-        fail();
-    }
-
-    @Test
-    void getInsurancePayout() {
-        fail();
-    }
-
-    @Test
-    void setInsurancePayout() {
-        fail();
-    }
-
-    @Test
-    void isEvenMoney() {
-        fail();
-    }
-
-    @Test
-    void setEvenMoney() {
-        fail();
-    }
-
-    @Test
-    void getDecksToLoad() {
-        fail();
-    }
-
-    @Test
-    void setDecksToLoad() {
-        fail();
-    }
-
-    @Test
-    void isClearShoeOnLoad() {
-        fail();
-    }
-
-    @Test
-    void setClearShoeOnLoad() {
-        fail();
-    }
-
-    @Test
-    void getPushRule() {
-        fail();
-    }
-
-    @Test
-    void setPushRule() {
-        fail();
-    }
-
-    @Test
-    void isDealerHitsSoft17() {
-        fail();
-    }
-
-    @Test
-    void setDealerHitsSoft17() {
-        fail();
-    }
-
-    @Test
-    void getBlackjackPayout() {
-        fail();
-    }
-
-    @Test
-    void setBlackjackPayout() {
-        fail();
-    }
-
-    @Test
-    void getMinBet() {
-        fail();
-    }
-
-    @Test
-    void setMinBet() {
-        fail();
-    }
 }
