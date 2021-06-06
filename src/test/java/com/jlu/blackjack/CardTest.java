93068fd414ba7730292fdd8d414bf76dcf3971b6
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 15:35:13 -0500

    Build test stubs for Card enum

---
+++ src/test/java/com/jlu/blackjack/CardTest.java
@@ -0,0 +1,28 @@
+package com.jlu.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    @Test
+    void handValue() {
+        fail();
+    }
+
+    @Test
+    void cardName() {
+        fail();
+    }
+
+    @Test
+    void values() {
+        fail();
+    }
+
+    @Test
+    void valueOf() {
+        fail();
+    }
+}
