6833eab797b64944ddb6262b6bc1a3a0df4b0092
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 16:30:58 -0500

    Add basic structure for Game class

--- src/main/java/com/jlu/blackjack/Game.java
+++ src/main/java/com/jlu/blackjack/Game.java
@@ -4,11 +4,69 @@ import java.util.List;
 
 public class Game {
 
+    private enum State{
+        PRE_DEAL,
+        DEALER_UP,
+        PLAYER_ACTIONS,
+        DEALER_REVEAL,
+        PAYOUT,
+        CLEANUP
+    }
+
+    private List<Participant> allParticipants;
+    private Dealer dealer;
     private List<Player> players;
+    private boolean keepPlaying;
 
-    public void state(){}
-    public void turn(){}
+    private Rules currentRules;
 
-    public void giveOptions(Hand playerHand){}
+    private State currentState;
+
+    /**
+     * Empty constructor, starts game with default rules
+     */
+    public Game(){
+        this(new Rules());
+    }
+
+    /**
+     * Constructor to start the game with specified rules set
+     * @param gameRules A rules object that specifies how the game will be played
+     */
+    public Game(Rules gameRules){
+        this.currentRules = gameRules;
+        this.currentState = State.PRE_DEAL;
+        this.keepPlaying = true;
+
+        mainLoop();
+    }
+
+    private void mainLoop(){
+        do{
+            System.out.println("Main game loop.");
+            keepPlaying=false;
+        } while(keepPlaying);
+    }
+
+    private void preDeal(){}
+    private void dealerUp(){}
+    private void playerActions(){}
+    private void dealerReveal(){}
+    private void payout(){}
+    private void cleanup(){}
+
+
+//    private void mainLoop() {
+//        switch (currentState){
+//            case PRE_DEAL -> {}
+//            case DEALER_UP -> {}
+//            case PLAYER_ACTIONS -> {}
+//            case DEALER_REVEAL -> {}
+//            case PAYOUT -> {}
+//            case CLEANUP -> {}
+//            default -> {}
+//        }
+//    }
+//
 
 }
