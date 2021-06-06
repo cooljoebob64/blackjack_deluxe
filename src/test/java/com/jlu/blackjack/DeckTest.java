5c0e8f0d5b9a394ea101234ceb1fdb1761cb041e
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 15:36:10 -0500

    Build test stubs for Deck class

---
+++ src/test/java/com/jlu/blackjack/DeckTest.java
@@ -0,0 +1,28 @@
+package com.jlu.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @Test
+    void reloadShoe() {
+        fail();
+    }
+
+    @Test
+    void dealCard() {
+        fail();
+    }
+
+    @Test
+    void shuffle() {
+        fail();
+    }
+
+    @Test
+    void burn() {
+        fail();
+    }
+}
