1bed43398a69f6e47e82653d026184fd970a6ea6
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 15:09:46 -0500

    Create Rules class fields and methods and test stubs

--- src/main/java/com/jlu/blackjack/Rules.java
+++ src/main/java/com/jlu/blackjack/Rules.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 public class Rules {
 
-    private List<Object> rules;
+    public enum pushRuleOptions{
+        NO_ACTION, PLAYER_WIN, PLAYER_LOSS
+    }
+
+    // Rule settings fields
     private boolean insurance; // Is insurance offered?
     private double insurancePayout; // Insurance payout rate?
     private boolean evenMoney; // Is Even Money offered?
@@ -12,13 +16,22 @@ public class Rules {
     private boolean clearShoeOnLoad; // Should the shoe be emptied first when reloaded?
     private int pushRule; // No action, player win, player loss?
     private boolean dealerHitsSoft17; // Does the dealer hit on a soft 17?
-    private int blackjackPayout; // Blackjack payout - 3:2, 2:1, custom?
+    private double blackjackPayout; // Blackjack payout - 3:2, 2:1, custom?
     private double minBet; // Minimum starting bet?
 
     /**
      * Empty constructor, employs most common default rules
      */
     public Rules(){
+        setInsurance(true);
+        setInsurancePayout(2.0);
+        setEvenMoney(true);
+        setDecksToLoad(2);
+        setClearShoeOnLoad(true);
+        setPushRule(0);
+        setDealerHitsSoft17(true);
+        setBlackjackPayout(1.5);
+        setMinBet(5.00);
     }
 
     /**
@@ -28,5 +41,79 @@ public class Rules {
     public String listRules(){
         return "";
     }
+
+    public boolean isInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(boolean insurance) {
+        this.insurance = insurance;
+    }
+
+    public double getInsurancePayout() {
+        return insurancePayout;
+    }
+
+    public void setInsurancePayout(double insurancePayout) {
+        this.insurancePayout = insurancePayout;
+    }
+
+    public boolean isEvenMoney() {
+        return evenMoney;
+    }
+
+    public void setEvenMoney(boolean evenMoney) {
+        this.evenMoney = evenMoney;
+    }
+
+    public int getDecksToLoad() {
+        return decksToLoad;
+    }
+
+    public void setDecksToLoad(int decksToLoad) {
+        this.decksToLoad = decksToLoad;
+    }
+
+    public boolean isClearShoeOnLoad() {
+        return clearShoeOnLoad;
+    }
+
+    public void setClearShoeOnLoad(boolean clearShoeOnLoad) {
+        this.clearShoeOnLoad = clearShoeOnLoad;
+    }
+
+    public int getPushRule() {
+        return pushRule;
+    }
+
+    public void setPushRule(int pushRule) {
+        this.pushRule = pushRule;
+    }
+
+    public boolean isDealerHitsSoft17() {
+        return dealerHitsSoft17;
+    }
+
+    public void setDealerHitsSoft17(boolean dealerHitsSoft17) {
+        this.dealerHitsSoft17 = dealerHitsSoft17;
+    }
+
+    public double getBlackjackPayout() {
+        return blackjackPayout;
+    }
+
+    public void setBlackjackPayout(double blackjackPayout) {
+        this.blackjackPayout = blackjackPayout;
+    }
+
+    public double getMinBet() {
+        return minBet;
+    }
+
+    public void setMinBet(double minBet) {
+        this.minBet = minBet;
+    }
+
+
 
 }
---
+++ src/test/java/com/jlu/blackjack/RulesTest.java
@@ -0,0 +1,103 @@
+package com.jlu.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RulesTest {
+
+    @Test
+    void listRules() {
+        fail();
+    }
+
+    @Test
+    void isInsurance() {
+        fail();
+    }
+
+    @Test
+    void setInsurance() {
+        fail();
+    }
+
+    @Test
+    void getInsurancePayout() {
+        fail();
+    }
+
+    @Test
+    void setInsurancePayout() {
+        fail();
+    }
+
+    @Test
+    void isEvenMoney() {
+        fail();
+    }
+
+    @Test
+    void setEvenMoney() {
+        fail();
+    }
+
+    @Test
+    void getDecksToLoad() {
+        fail();
+    }
+
+    @Test
+    void setDecksToLoad() {
+        fail();
+    }
+
+    @Test
+    void isClearShoeOnLoad() {
+        fail();
+    }
+
+    @Test
+    void setClearShoeOnLoad() {
+        fail();
+    }
+
+    @Test
+    void getPushRule() {
+        fail();
+    }
+
+    @Test
+    void setPushRule() {
+        fail();
+    }
+
+    @Test
+    void isDealerHitsSoft17() {
+        fail();
+    }
+
+    @Test
+    void setDealerHitsSoft17() {
+        fail();
+    }
+
+    @Test
+    void getBlackjackPayout() {
+        fail();
+    }
+
+    @Test
+    void setBlackjackPayout() {
+        fail();
+    }
+
+    @Test
+    void getMinBet() {
+        fail();
+    }
+
+    @Test
+    void setMinBet() {
+        fail();
+    }
+}
