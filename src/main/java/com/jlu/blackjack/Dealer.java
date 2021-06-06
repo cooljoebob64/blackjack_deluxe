589b73792104055ffb011899b7c8a69f6b34f06c
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 16:04:29 -0500

    Update Dealer to implement Participant

--- src/main/java/com/jlu/blackjack/Dealer.java
+++ src/main/java/com/jlu/blackjack/Dealer.java
@@ -1,4 +1,23 @@
 package com.jlu.blackjack;
 
-public class Dealer extends Player{
+public class Dealer implements Participant{
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean isStillActive() {
+        return false;
+    }
+
+    @Override
+    public Hand getCurrentHand() {
+        return null;
+    }
+
+    @Override
+    public Hand getSplitHand() {
+        return null;
+    }
 }
