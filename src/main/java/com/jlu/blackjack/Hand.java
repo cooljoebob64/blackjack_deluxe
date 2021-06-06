5bda9c55d6758005d9db282603458d87fcfc7001
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 15:42:44 -0500

    Create method stubs for Hand class

--- src/main/java/com/jlu/blackjack/Hand.java
+++ src/main/java/com/jlu/blackjack/Hand.java
@@ -1,4 +1,14 @@
 package com.jlu.blackjack;
 
+import java.util.List;
+
 public class Hand {
+
+    List<Card> currentCards;
+
+    void addCard(Card newCard){}
+    void possibleValues(){}
+    void highestNonBust(){}
+    void clearHand(){}
+
 }
