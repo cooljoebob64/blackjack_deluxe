9deead8c6409609729c55fd5a588a6289600ff5a
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 16:01:14 -0500

    Implements placeholders of Participant methods in Player class

--- src/main/java/com/jlu/blackjack/Player.java
+++ src/main/java/com/jlu/blackjack/Player.java
@@ -1,4 +1,26 @@
 package com.jlu.blackjack;
 
-public class Player {
+public class Player implements Participant {
+
+    private String playerName;
+
+    @Override
+    public String getName() {
+        return playerName;
+    }
+
+    @Override
+    public boolean isStillActive() {
+        return false;
+    }
+
+    @Override
+    public Hand getCurrentHand() {
+        return null;
+    }
+
+    @Override
+    public Hand getSplitHand() {
+        return null;
+    }
 }
