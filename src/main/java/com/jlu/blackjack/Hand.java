b043a488b175af5cd47003174908093c0b3ca148
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 16:03:44 -0500

    Add method and field to track Main or Split hand in Hand class

--- src/main/java/com/jlu/blackjack/Hand.java
+++ src/main/java/com/jlu/blackjack/Hand.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class Hand {
 
     List<Card> currentCards;
+    boolean mainHand;
 
     void addCard(Card newCard){}
     void possibleValues(){}
     void highestNonBust(){}
+    void isMainHand(){}
     void clearHand(){}
     void getActions(){}
 
