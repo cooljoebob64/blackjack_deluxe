031adeac7d0b6ad9dabee51221147dbf137463db
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-06 17:27:44 -0500

    Generate tests for Hand class

---
+++ src/test/java/com/jlu/blackjack/HandTest.java
@@ -0,0 +1,38 @@
+package com.jlu.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandTest {
+
+    @Test
+    void addCard() {
+        fail();
+    }
+
+    @Test
+    void possibleValues() {
+        fail();
+    }
+
+    @Test
+    void highestNonBust() {
+        fail();
+    }
+
+    @Test
+    void isMainHand() {
+        fail();
+    }
+
+    @Test
+    void clearHand() {
+        fail();
+    }
+
+    @Test
+    void getActions() {
+        fail();
+    }
+}
