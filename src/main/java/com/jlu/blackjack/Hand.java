450c861398130f17258c1518d699958acae9f804
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-08 14:37:06 -0500

    Add tests for Hand class

--- src/main/java/com/jlu/blackjack/Hand.java
+++ src/main/java/com/jlu/blackjack/Hand.java
@@ -1,22 +1,54 @@
 package com.jlu.blackjack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
 
-    List<Card> currentCards;
-    boolean mainHand;
-
-    Game currentGame;
-    public Hand(Game currentGame){
-        this.currentGame = currentGame;
+    public Hand(Rules currentRules){
+        this.currentRules = currentRules;
     }
 
+    public List<Card> getCurrentCards() {
+        return currentCards;
+    }
+
+    public void setCurrentCards(List<Card> currentCards) {
+        this.currentCards = currentCards;
+    }
+
+
+
+
+    public Rules getCurrentRules() {
+        return currentRules;
+    }
+
+    public void setCurrentRules(Rules currentRules) {
+        this.currentRules = currentRules;
+    }
+
+    private Rules currentRules;
+    private List<Card> currentCards;
+    private boolean mainHand;
+
     void addCard(Card newCard){}
-    void possibleValues(){}
-    void highestNonBust(){}
+    List<Integer> possibleValues(){
+        return new ArrayList<>();
+    }
+    int highestNonBust(){
+        return 0;
+    }
     void isMainHand(){}
+    public void setMainHand(boolean mainHand) {
+        this.mainHand = mainHand;
+    }
     void clearHand(){}
-    void getActions(){}
+    List<Action> getActions(){
+        return new ArrayList<>();
+    }
+    void addAction(Action newAction){
+
+    }
 
 }
--- src/test/java/com/jlu/blackjack/HandTest.java
+++ src/test/java/com/jlu/blackjack/HandTest.java
@@ -1,38 +1,119 @@
 package com.jlu.blackjack;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HandTest {
 
-    @Test
-    void addCard() {
-        fail();
+    Rules testRules;
+    Hand testHand;
+
+    @BeforeEach
+    void setUp(){
+        testRules = new Rules();
+        testHand = new Hand(testRules);
     }
 
     @Test
-    void possibleValues() {
-        fail();
+    void addCard() {
+        int expectedSize = 3;
+        int actual;
+
+        int beforeSize = testHand.getCurrentCards().size();
+        testHand.addCard(Card.D2);
+        testHand.addCard(Card.D2);
+        testHand.addCard(Card.D2);
+        int afterSize = testHand.getCurrentCards().size();
+        actual = afterSize - beforeSize;
+
+        assertEquals(expectedSize, actual);
+    }
+
+    @Test
+    void possibleValuesEmpty() {
+        List<Integer> actualEmptyValue;
+
+        testHand.clearHand();
+        actualEmptyValue = testHand.possibleValues();
+
+        assertEquals(0, actualEmptyValue.size());
+
+    }
+
+    @Test
+    void possibleValuesTwoCards() {
+        int expectedSize = 1;
+        int expectedValue = 5;
+        List<Integer> actualTwoCardValue;
+
+        testHand.clearHand();
+        testHand.addCard(Card.D2);
+        testHand.addCard(Card.D3);
+        actualTwoCardValue = testHand.possibleValues();
+
+        assertEquals(expectedSize, actualTwoCardValue.size());
+        assertEquals(expectedValue, actualTwoCardValue.get(0));
+    }
+
+    @Test
+    void possibleValuesAce() {
+        int expectedSize = 2;
+        int expectedFirstValue = 3;
+        int expectedSecondValue = 13;
+        List<Integer> actualAceValue;
+
+        testHand.clearHand();
+        testHand.addCard(Card.D2);
+        testHand.addCard(Card.DA);
+        actualAceValue = testHand.possibleValues();
+
+        assertEquals(expectedSize, actualAceValue.size());
+        assertEquals(expectedFirstValue, actualAceValue.get(0));
+        assertEquals(expectedSecondValue, actualAceValue.get(1));
     }
 
     @Test
     void highestNonBust() {
-        fail();
-    }
+        int expectedHighestValue = 19;
+        int actualHighestValue;
 
-    @Test
-    void isMainHand() {
-        fail();
+        testHand.clearHand();
+        testHand.addCard(Card.D3);
+        testHand.addCard(Card.D5);
+        testHand.addCard(Card.DK);
+        testHand.addCard(Card.DA);
+        actualHighestValue = testHand.highestNonBust();
+
+        assertEquals(expectedHighestValue, actualHighestValue);
     }
 
     @Test
     void clearHand() {
-        fail();
+        int expectedSize = 0;
+        int actualSize;
+
+        testHand.clearHand();
+        testHand.addCard(Card.D2);
+        testHand.clearHand();
+        actualSize = testHand.getCurrentCards().size();
+
+        assertEquals(expectedSize, actualSize);
     }
 
     @Test
     void getActions() {
-        fail();
+        List<Action> expectedActions = new ArrayList<>();
+        expectedActions.add(Action.HIT);
+        expectedActions.add(Action.STAND);
+        expectedActions.add(Action.DOUBLE);
+
+        List<Action> actualActions = testHand.getActions();
+
+        assertEquals(expectedActions, actualActions);
     }
 }
